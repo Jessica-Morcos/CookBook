32d9c6b1d02a5a3772ebbb07c7975c1bbe8381ec
Jessica-Morcos <132419894+Jessica-Morcos@users.noreply.github.com>
2024-07-12 23:27:10 -0400

    Initial Commit

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Jessica Morcos  on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
